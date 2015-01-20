8257546cb777d210a7bc20c35f98a010fb3f0b2f
Steven D. Lander <steven.lander@rgi-corp.com>
2015-01-20 13:18:26 -0500

    created build script, removed system deps

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mvn install:install-file -Dfile="Gdal2Tiles/lib/gdal.jar" -DgroupId=org.gdal -DartifactId=gdal -Dversion=1.11.0 -Dpackaging=jar
+mvn install:install-file -Dfile="GeoViewer/lib/JMapViewer-1.05.jar" -DgroupId=org.openstreetmap -DartifactId=JMapViewer -Dversion=1.05 -Dpackaging=jar
+mvn test
