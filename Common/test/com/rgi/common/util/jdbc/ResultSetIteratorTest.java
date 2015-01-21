3bb1d714ebc52d25ca5d30d0e299b43c51b14cf2
Steven D. Lander <steven.lander@rgi-corp.com>
2015-01-21 14:54:35 -0500

    Adding common jdbc tests

---
+++ Common/test/com/rgi/common/util/jdbc/ResultSetIteratorTest.java
@@ -0,0 +1,69 @@
+package com.rgi.common.util.jdbc;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.function.Function;
+
+import org.junit.Test;
+
+import com.mockrunner.mock.jdbc.MockResultSet;
+
+public class ResultSetIteratorTest<T> {
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void ResultSetIteratorConstructorNullResult() {
+		Function<ResultSet, T> mappingFunction = null;
+		ResultSetIterator rsi = new ResultSetIterator(null, mappingFunction);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void ResultSetIteratorConstructorNullFunction() {
+		MockResultSet rs = new MockResultSet("myMock");
+		rs.addColumn("columnA", new Integer[]{1});
+		ResultSetIterator rsi = new ResultSetIterator(rs, null);
+	}
+	
+	@Test
+	public void ResultSetInteratorConstructor() {
+		MockResultSet resultSet = new MockResultSet("myMock");
+		resultSet.addColumn("columnA", new Integer[]{1});
+		ResultSetIterator rsi = new ResultSetIterator(resultSet, rs -> rs);
+		assertFalse(resultSet.isClosed());
+	}
+	
+	@Test
+	public void ResultSetHasNext() {
+		MockResultSet resultSet = new MockResultSet("myMock");
+		resultSet.addColumn("columnA", new Integer[]{1});
+		ResultSetIterator rsi = new ResultSetIterator(resultSet, rs -> rs);
+		assertTrue(rsi.hasNext());
+	}
+	
+	@Test
+	public void ResultSetDoesNotHaveNext() {
+		MockResultSet resultSet = new MockResultSet("myMock");
+		ResultSetIterator rsi = new ResultSetIterator(resultSet, rs -> rs);
+		assertFalse(rsi.hasNext());
+	}
+
+	@Test
+	public void ResultSetDoesNotHaveNextClosed() throws SQLException {
+		MockResultSet resultSet = new MockResultSet("myMock");
+		ResultSetIterator rsi = new ResultSetIterator(resultSet, rs -> rs);
+		resultSet.addColumn("columnA", new Integer[]{1});
+		resultSet.close();
+		assertFalse(rsi.hasNext());
+	}
+	
+	@Test
+	public void ResultSetDoesNotHaveNextLastItem() {
+		MockResultSet resultSet = new MockResultSet("myMock");
+		resultSet.addColumn("columnA", new Integer[]{1});
+		ResultSetIterator rsi = new ResultSetIterator(resultSet, rs -> rs);
+		rsi.next();
+		assertFalse(rsi.hasNext());
+	}
+}
