e25109bfd6f186df92a6fc633d87850018d4493f
LLambertRGi <luke.lambert@rgi-corp.com>
2015-01-28 21:56:31 -0500

    Fixed warnings

--- Geopackage/test/com/rgi/geopackage/GeoPackageExtensionsAPITest.java
+++ Geopackage/test/com/rgi/geopackage/GeoPackageExtensionsAPITest.java
@@ -1,48 +1,37 @@
 package com.rgi.geopackage;
 
-import java.io.File;
-import java.nio.file.FileSystems;
-import java.util.Random;
-
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
-public class GeoPackageExtensionsAPITest 
+public class GeoPackageExtensionsAPITest
 {
-	
+
 	@Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
-    private final Random randomGenerator = new Random();
-    
-   
-    
-    
-    
-    
-    
-    
-    private String getRanString(final int length)
-    {
-        final String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        final char[] text = new char[length];
-        for (int i = 0; i < length; i++)
-        {
-            text[i] = characters.charAt(this.randomGenerator.nextInt(characters.length()));
-        }
-        return new String(text);
-    }
+    //private final Random randomGenerator = new Random();
 
-    private File getRandomFile(final int length)
-    {
-        File testFile;
 
-        do
-        {
-            testFile = new File(String.format(FileSystems.getDefault().getPath(this.getRanString(length)).toString() + ".gpkg"));
-        }
-        while (testFile.exists());
-
-        return testFile;
-    }
-
+//    private String getRanString(final int length)
+//    {
+//        final String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//        final char[] text = new char[length];
+//        for (int i = 0; i < length; i++)
+//        {
+//            text[i] = characters.charAt(this.randomGenerator.nextInt(characters.length()));
+//        }
+//        return new String(text);
+//    }
+//
+//    private File getRandomFile(final int length)
+//    {
+//        File testFile;
+//
+//        do
+//        {
+//            testFile = new File(String.format(FileSystems.getDefault().getPath(this.getRanString(length)).toString() + ".gpkg"));
+//        }
+//        while (testFile.exists());
+//
+//        return testFile;
+//    }
 }
