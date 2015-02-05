ec5f12978206b1cfaef706fca79aa13a93cf7e1a
LLambertRGi <luke.lambert@rgi-corp.com>
2015-02-04 21:35:52 -0500

    Removed unused code

--- Common/src/com/rgi/common/tile/TileOrigin.java
+++ Common/src/com/rgi/common/tile/TileOrigin.java
@@ -62,22 +62,6 @@ public enum TileOrigin
                                 this.transformHorizontal(toOrigin, tileCoordinate.getX().intValue(), matrixDimensions.getWidth()));
     }
 
-    /**
-     * @return the horizontal
-     */
-    public int getHorizontal()
-    {
-        return this.horizontal;
-    }
-
-    /**
-     * @return the vertical
-     */
-    public int getVertical()
-    {
-        return this.vertical;
-    }
-
     public Coordinate<Integer> transform(final TileOrigin toOrigin, final int tileY, final int tileX, final TileMatrixDimensions matrixDimensions)
     {
         if(toOrigin == null)
