5865cdcfe6b519799e712b97ee6567daad6edd2a
LLambertRGi <luke.lambert@rgi-corp.com>
2015-03-10 11:04:46 -0400

    Abstracting common tiler/packager UI elements

--- RGISuite/src/com/rgi/suite/PackagerWindow.java
+++ RGISuite/src/com/rgi/suite/PackagerWindow.java
@@ -1,12 +1,5 @@
 package com.rgi.suite;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowEvent;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
@@ -16,24 +9,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.activation.MimeType;
-import javax.swing.AbstractAction;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import store.GeoPackageWriter;
 import utility.TileStoreUtility;
 import utility.TileStoreUtility.TileStoreTraits;
 
 import com.rgi.common.Range;
-import com.rgi.common.coordinate.CoordinateReferenceSystem;
-import com.rgi.common.coordinate.referencesystem.profile.CrsProfileFactory;
 import com.rgi.common.tile.scheme.TileScheme;
 import com.rgi.common.tile.scheme.ZoomTimesTwo;
 import com.rgi.common.tile.store.TileHandle;
@@ -51,24 +33,9 @@ import com.rgi.packager.Packager;
  * @author Luke D. Lambert
  *
  */
-public class PackagerWindow extends JFrame
+public class PackagerWindow extends TileStoreCreationWindow
 {
-    private static final long serialVersionUID = -3488202344008846021L;
-
-    private final Settings settings;
-
-    private final JPanel contentPane = new JPanel(new GridBagLayout());
-    private final JPanel navPane     = new JPanel(new GridBagLayout());
-
-    private final JTextField inputFileName      = new JTextField();
-    private final JTextField tileSetName        = new JTextField();
-    private final JTextField tileSetDescription = new JTextField();
-    private final JTextField outputFileName     = new JTextField();
-
-    private final JComboBox<CoordinateReferenceSystem> inputCrs = new JComboBox<>(new DefaultComboBoxModel<>(CrsProfileFactory.getSupportedCoordinateReferenceSystems()
-                                                                                                                              .stream()
-                                                                                                                              .sorted()
-                                                                                                                              .toArray(CoordinateReferenceSystem[]::new)));
+    private static final long serialVersionUID = -6391017242212287246L;
 
     private static final String LastInputLocationSettingName = "package.lastInputLocation";
 
@@ -80,129 +47,19 @@ public class PackagerWindow extends JFrame
      */
     public PackagerWindow(final Settings settings)
     {
-        this.setTitle("Packaging Settings");
-        this.setLayout(new BorderLayout());
-        this.setPreferredSize(new Dimension(600, 240));
-        this.setResizable(false);
+        super("Packaging", settings, LastInputLocationSettingName);
 
-        this.settings = settings;
-
-        this.buildContentPane();
-        this.buildNavPane();
-
-        this.add(this.contentPane, BorderLayout.CENTER);
-        this.add(this.navPane,     BorderLayout.SOUTH);
-    }
-
-    private void buildContentPane()
-    {
-        final JButton inputFileNameButton = new JButton("\u2026");
-
-        this.inputFileName.setEnabled(false);
-        this.inputCrs     .setEnabled(false);
-
-        inputFileNameButton.addActionListener(e -> { final String startDirectory = PackagerWindow.this.settings.get(LastInputLocationSettingName, SettingsWindow.DefaultOutputLocation);
-
-                                                     final JFileChooser fileChooser = new JFileChooser(new File(startDirectory));
-
-                                                     fileChooser.setMultiSelectionEnabled(false);
-                                                     fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-
-                                                     final int option = fileChooser.showOpenDialog(this.contentPane);
-
-                                                     if(option == JFileChooser.APPROVE_OPTION)
-                                                     {
-                                                         final File file = fileChooser.getSelectedFile();
-
-                                                         try
-                                                         {
-                                                             PackagerWindow.this.inputFileChanged(file);
-                                                         }
-                                                         catch(final Exception ex)
-                                                         {
-                                                             JOptionPane.showMessageDialog(this,
-                                                                                           ex.getMessage(),
-                                                                                           "Packaging",
-                                                                                           JOptionPane.ERROR_MESSAGE);
-                                                         }
-                                                     }
-                                                    });
-
-        final int    anchor = GridBagConstraints.WEST;
-        final int    fill   = GridBagConstraints.HORIZONTAL;
-        final Insets insets = new Insets(5, 5, 5, 5);
-
-        // Input tile store file
-        this.contentPane.add(new JLabel("Input tile store:"),     new GridBagConstraints(0, 0, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-        this.contentPane.add(this.inputFileName,                  new GridBagConstraints(1, 0, 1, 1, 1, 1, anchor, fill, insets, 0, 0));
-        this.contentPane.add(inputFileNameButton,                 new GridBagConstraints(2, 0, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-
-        // Input CRS
-        this.contentPane.add(new JLabel("Input CRS:"),            new GridBagConstraints(0, 1, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-        this.contentPane.add(this.inputCrs,                       new GridBagConstraints(1, 1, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-
-        // Tile set name
-        this.contentPane.add(new JLabel("Tile Set Name:"),        new GridBagConstraints(0, 2, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-        this.contentPane.add(this.tileSetName,                    new GridBagConstraints(1, 2, 1, 1, 1, 1, anchor, fill, insets, 0, 0));
-
-        // Tile set description
-        this.contentPane.add(new JLabel("Tile Set Description:"), new GridBagConstraints(0, 3, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-        this.contentPane.add(this.tileSetDescription,             new GridBagConstraints(1, 3, 1, 1, 1, 1, anchor, fill, insets, 0, 0));
-
-        // Output file name
-        this.contentPane.add(new JLabel("Output File Name:"),     new GridBagConstraints(0, 4, 1, 1, 0, 1, anchor, fill, insets, 0, 0));
-        this.contentPane.add(this.outputFileName,                 new GridBagConstraints(1, 4, 1, 1, 1, 1, anchor, fill, insets, 0, 0));
-    }
-
-    private void buildNavPane()
-    {
-         final JButton okButton = new JButton(new AbstractAction()
-                                             {
-                                                 private static final long serialVersionUID = -5059914828508260038L;
-
-                                                 @Override
-                                                 public void actionPerformed(final ActionEvent event)
-                                                 {
-                                                     try
-                                                    {
-                                                        PackagerWindow.this.makePackage();
-                                                        PackagerWindow.this.closeFrame();
-                                                    }
-                                                    catch(final Exception ex)
-                                                    {
-                                                        ex.printStackTrace();
-                                                        JOptionPane.showMessageDialog(PackagerWindow.this, "Packaging", "An error has occurred: " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
-                                                    }
-                                                 }
+        this.okButton.addActionListener(e -> { try
+                                               {
+                                                   PackagerWindow.this.makePackage();
+                                                   PackagerWindow.this.closeFrame();
+                                               }
+                                               catch(final Exception ex)
+                                               {
+                                                   ex.printStackTrace();
+                                                   JOptionPane.showMessageDialog(PackagerWindow.this, this.processName, "An error has occurred: " + ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+                                               }
                                              });
-
-        okButton.setText("OK");
-
-        final JButton cancelButton = new JButton(new AbstractAction()
-                                                 {
-                                                     private static final long serialVersionUID = -4389758606354266920L;
-
-                                                     @Override
-                                                     public void actionPerformed(final ActionEvent event)
-                                                     {
-                                                         PackagerWindow.this.closeFrame();
-                                                     }
-                                                 });
-
-        cancelButton.setText("Cancel");
-
-        // Add buttons to pane
-        final Insets insets = new Insets(10, 10, 10, 10);
-        final int    fill   = GridBagConstraints.NONE;
-
-        this.navPane.add(okButton,     new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.EAST, fill, insets, 0, 0));
-        this.navPane.add(cancelButton, new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.EAST, fill, insets, 0, 0));
-    }
-
-
-    private void closeFrame()
-    {
-        this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
     }
 
     private void makePackage() throws Exception
@@ -239,7 +96,8 @@ public class PackagerWindow extends JFrame
         }
     }
 
-    private void inputFileChanged(final File file) throws TileStoreException
+    @Override
+    protected void inputFileChanged(final File file) throws TileStoreException
     {
         this.settings.set(LastInputLocationSettingName, file.getParent());
         this.settings.save();
