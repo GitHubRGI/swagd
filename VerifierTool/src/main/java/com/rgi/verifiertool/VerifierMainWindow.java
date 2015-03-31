a3fea2b0dbcb5f7d8758d64a063006dfb2527e01
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-03-31 10:31:27 -0400

    Adding info about the application in hyperlinks

--- VerifierTool/src/main/java/com/rgi/verifiertool/VerifierMainWindow.java
+++ VerifierTool/src/main/java/com/rgi/verifiertool/VerifierMainWindow.java
@@ -29,18 +29,18 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.effect.Light.Distant;
 import javafx.scene.effect.Lighting;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 /**
@@ -64,11 +64,15 @@ public class VerifierMainWindow extends Application
     public void start(final Stage primaryStage) throws Exception
     {
         //Set the window up
-        BorderPane layout = new BorderPane();
+        BorderPane  layout = new BorderPane();
         Scene       scene = new Scene(layout, 551, 400);
+        GridPane    bottomGrid = new GridPane();
+
+        layout.setBottom(bottomGrid);
+        bottomGrid.setHgap(100);
 
         primaryStage.setTitle("GeoPackage Verifier Tool");
-
+        //set drag here message
         final Text dragHereMessage = new Text("Drag GeoPackage Files Here.");
         dragHereMessage.setFill(Color.DARKBLUE);
         dragHereMessage.setFont(Font.font(null, FontWeight.BOLD, 30));
@@ -80,27 +84,44 @@ public class VerifierMainWindow extends Application
         lighting.setSurfaceScale(4.0f);
 
         dragHereMessage.setEffect(lighting);
-
-        Text applicationInfo = new Text();//TODO
-        Hyperlink geoPackageLink = new Hyperlink("GeoPackage Specification");
-        layout.setBottom(geoPackageLink);
-        WebView browser = new WebView();
-        WebEngine webEngine = browser.getEngine();
-        geoPackageLink.setOnAction(e -> {
-            //TODO open in user browser
-                                            Stage stage = new Stage();
-                                            ScrollPane root = new ScrollPane();
-                                            Scene browserScene = new Scene(root);
-                                            webEngine.load("http://www.geopackage.org/spec/");
-                                            root.setFitToWidth(true);
-                                            root.setFitToHeight(true);
-                                            root.setContent(browser);
-                                            stage.setScene(browserScene);
-                                            stage.show();
-                                        }
-                                        );
-        primaryStage.setResizable(false);
         layout.setCenter(dragHereMessage);
+
+        //Create Link to SWAGD github
+        Hyperlink swagdInfo = new Hyperlink("SWAGD Project");
+        bottomGrid.add(swagdInfo, 0, 0);
+        swagdInfo.setOnAction(e-> this.getHostServices().showDocument("https://github.com/GitHubRGI/swagd"));
+
+        //create link to GeoPackage specification Document
+        Hyperlink geoPackageLink = new Hyperlink("GeoPackage Specification");
+        bottomGrid.add(geoPackageLink, 1, 0);
+        geoPackageLink.setOnAction(e -> this.getHostServices().showDocument("http://www.geopackage.org/spec/"));
+
+        //add an about this application link
+        Hyperlink verifierToolInfo = new Hyperlink("About application");
+        bottomGrid.add(verifierToolInfo, 2, 0);
+        verifierToolInfo.setOnAction(e -> {
+
+                                                Text     title  = new Text("GeoPackage Verifier Tool\n");
+                                                title.setFill(Color.DARKBLUE);
+                                                title.setFont(Font.font(null, FontWeight.EXTRA_BOLD, 20));
+                                                title.setTextAlignment(TextAlignment.CENTER);
+                                                Text     company = new Text("Reinventing Geospatial Inc.\n\n");
+                                                company.setFont(Font.font(null, FontWeight.BOLD, 18));
+
+                                                Text     about   = new Text("This application will verify GeoPackages with Raster Tile data for the GeoPackage Encoding Standard Version 1.0.  "
+                                                                          + "This Verification Tool will test the GeoPackage Core, GeoPackage Tiles, GeoPackage Extensions, GeoPackage Schema, "
+                                                                          + "and GeoPackage Metadata requirements.  This will test a GeoPackage file against the Requirements referenced in the Encoding Standard.   "
+                                                                          + "This will not verify GeoPackages with Feature data.");
+                                                about.setFont(Font.font(null, FontWeight.THIN, 14));
+
+                                                Stage infoStage = new Stage();
+                                                TextFlow text   = new TextFlow(title, company, about);
+                                                Scene infoScene = new Scene(text, 400, 230);
+
+                                                infoStage.setScene(infoScene);
+                                                infoStage.show();
+
+                                            });
 
         //create the even that drags the file over
         scene.setOnDragOver(event ->
@@ -133,9 +154,10 @@ public class VerifierMainWindow extends Application
         });
 
         //show the window
+
+        primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(event -> Platform.exit());
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        primaryStage.setOnCloseRequest(event -> Platform.exit());
     }
 }
