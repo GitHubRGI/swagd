f1ecd196640aa8eb15aef8615d26942f9492d9b5
LLambertRGi <luke.lambert@rgi-corp.com>
2015-04-08 14:40:48 -0400

    Not sure how the package name changed...

---
+++ VerifierTool/src/com/rgi/verifiertool/FileVerificationPane.java
@@ -0,0 +1,156 @@
+package com.rgi.verifiertool;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.beans.value.ChangeListener;
+import javafx.concurrent.Task;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import com.rgi.common.util.functional.ThrowingConsumer;
+import com.rgi.geopackage.GeoPackage;
+import com.rgi.geopackage.GeoPackage.OpenMode;
+import com.rgi.geopackage.verification.VerificationIssue;
+import com.rgi.geopackage.verification.VerificationLevel;
+
+/**
+ * Pane representing a single GeoPackage file's verification output
+ *
+ * @author Luke Lambert
+ * @author Jenifer Cochran
+ *
+ */
+public class FileVerificationPane extends TitledPane
+{
+    private final VBox content = new VBox();
+    private final ContextMenu deleteMenu = new ContextMenu();
+    private VBox parent;
+
+    /**
+     * Constructor
+     *
+     * @param geoPackageFile
+     *             File handle to a GeoPackage file
+     */
+    public FileVerificationPane(final File geoPackageFile)
+    {
+        if(geoPackageFile == null || !geoPackageFile.canRead())
+        {
+            throw new IllegalArgumentException("GeoPackage file may not be null, and must be a valid filename");
+        }
+
+        this.setAnimated(false);
+        this.setText(geoPackageFile.getName());
+        this.setPrettyText();
+        this.content.setStyle(String.format("-fx-background-color: %s;", Style.white.getHex()));
+        this.createContextMenu();
+        this.setOnMousePressed(e -> this.createDeleteListener(e));
+
+        final List<SubsystemVerificationPane> subsystems = Arrays.asList(new SubsystemVerificationPane("Core",       (geoPackage) -> geoPackage.core()      .getVerificationIssues(geoPackage.getFile(), VerificationLevel.Full)),
+                                                                         //new SubsystemVerificationPane("Features",   (geoPackage) -> Collections.emptyList()),
+                                                                         new SubsystemVerificationPane("Tiles",      (geoPackage) -> geoPackage.tiles()     .getVerificationIssues(VerificationLevel.Full)),
+                                                                         new SubsystemVerificationPane("Extensions", (geoPackage) -> geoPackage.extensions().getVerificationIssues(VerificationLevel.Full)),
+                                                                         new SubsystemVerificationPane("Schema",     (geoPackage) -> geoPackage.schema()    .getVerificationIssues(VerificationLevel.Full)),
+                                                                         new SubsystemVerificationPane("Metadata",   (geoPackage) -> geoPackage.metadata()  .getVerificationIssues(VerificationLevel.Full)));
+
+        this.content.getChildren().addAll(subsystems);
+
+        final Thread mainThread = new Thread(new Task<Void>()
+                                             {
+                                                 @Override
+                                                 protected Void call() throws Exception
+                                                 {
+                                                     try(final GeoPackage geoPackage = new GeoPackage(geoPackageFile, VerificationLevel.None, OpenMode.Open))
+                                                     {
+                                                         final List<Thread> updateThreads = subsystems.stream()
+                                                                                                      .map(subsystem -> new Thread(FileVerificationPane.createTask(subsystem, geoPackage)))
+                                                                                                      .collect(Collectors.toList());
+
+                                                         updateThreads.forEach(thread -> thread.start());
+
+                                                         updateThreads.forEach((ThrowingConsumer<Thread>)(thread -> thread.join()));
+                                                     }
+                                                     catch(final Exception ex)
+                                                     {
+                                                         throw new RuntimeException(ex);
+                                                     }
+
+                                                     return null;
+                                                 }
+                                             });
+        mainThread.start();
+    }
+
+    private void createDeleteListener(final MouseEvent e)
+    {
+        if(e.isSecondaryButtonDown())
+        {
+            this.deleteMenu.show(this, e.getScreenX(), e.getScreenY());
+        }
+    }
+
+    /**
+     * @param parent the parent that this pane is added to
+     */
+    public void setParent(final VBox parent)
+    {
+        this.parent = parent;
+    }
+
+    private void createContextMenu()
+    {
+        final MenuItem remove = new MenuItem("Remove");
+        final MenuItem cancel = new MenuItem("Cancel");
+        this.deleteMenu.getItems().addAll(remove, cancel);
+
+        remove.setOnAction(e -> {
+                                if(this.parent != null)
+                                  {
+                                      this.parent.getChildren().remove(this);
+                                  }
+                                });
+    }
+
+    private void setPrettyText()
+    {
+        this.setContent(this.content);
+        this.setTextFill(Style.brightBlue.toColor());
+        this.setFont(Font.font(Style.getFont(), FontWeight.BOLD, 18));
+        this.setStyle(String.format("-fx-body-color: %s;", Style.white.getHex()));
+    }
+
+    private static Task<Collection<VerificationIssue>> createTask(final SubsystemVerificationPane subsystemVerificationPane, final GeoPackage geoPackage)
+    {
+        final Task<Collection<VerificationIssue>> task = new Task<Collection<VerificationIssue>>()
+                                                         {
+                                                             @Override
+                                                             protected Collection<VerificationIssue> call() throws Exception
+                                                             {
+                                                                 try
+                                                                 {
+                                                                     final Collection<VerificationIssue> messages = subsystemVerificationPane.getIssues(geoPackage);
+                                                                     this.updateValue(messages);
+                                                                     return messages;
+                                                                 }
+                                                                 catch(final Exception ex)
+                                                                 {
+                                                                     return null;
+                                                                 }
+
+                                                             }
+                                                         };
+
+        task.valueProperty().addListener((ChangeListener<Collection<VerificationIssue>>)(observable, oldValue, newValue) -> subsystemVerificationPane.update(newValue));
+
+        return task;
+    }
+}
---
+++ VerifierTool/src/com/rgi/verifiertool/SubsystemVerificationPane.java
@@ -0,0 +1,283 @@
+package com.rgi.verifiertool;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import com.rgi.common.util.functional.ThrowingFunction;
+import com.rgi.geopackage.GeoPackage;
+import com.rgi.geopackage.verification.Requirement;
+import com.rgi.geopackage.verification.Severity;
+import com.rgi.geopackage.verification.VerificationIssue;
+
+/**
+ * Pane representing a single GeoPackage subsystem's verification output
+ *
+ * @author Luke Lambert
+ * @author Jenifer Cochran
+ *
+ */
+public class SubsystemVerificationPane extends VBox
+{
+    private boolean hasIssues;
+
+    private final String subsystemName;
+
+    private final ThrowingFunction<GeoPackage, Collection<VerificationIssue>> issuesFunction;
+
+    private final GridPane gridPane = new GridPane();
+
+    private final ProgressIndicator progressIndicator = new ProgressIndicator();
+
+    /**
+     * Constructor
+     *
+     * @param subsystemName
+     *             Name of GeoPackage subsystem
+     * @param issuesFunction
+     *             Function to be called later that gather's the verification
+     *             messages from the subsystem
+     */
+    public SubsystemVerificationPane(final String subsystemName, final ThrowingFunction<GeoPackage, Collection<VerificationIssue>> issuesFunction)
+    {
+        super(3);
+        if(subsystemName == null || subsystemName.isEmpty())
+        {
+            throw new IllegalArgumentException("Subsystem name may not be null or empty");
+        }
+
+        if(issuesFunction == null)
+        {
+            throw new IllegalArgumentException("Funtional callback to get verification issue messages may not be null");
+        }
+
+        this.subsystemName  = subsystemName;
+        this.issuesFunction = issuesFunction;
+        this.setStyle(String.format("-fx-background-color: %s;", Style.white.getHex()));
+        this.setPadding(new Insets(7));
+
+        this.progressIndicator.setMinSize(25, 25);
+        this.progressIndicator.setMaxSize(25, 25);
+
+        final Label subsystemLabel = this.prettyLabel();
+
+        this.gridPane.add(subsystemLabel, 0, 0);
+        this.gridPane.add(this.progressIndicator, 1, 0);
+        //Columns for the top label
+        final ColumnConstraints columnLeft   = new ColumnConstraints(90);
+        final ColumnConstraints columnCenter = new ColumnConstraints(360);
+        final ColumnConstraints columnRight  = new ColumnConstraints(25, 25, 25);
+
+        this.gridPane.getColumnConstraints().addAll(columnLeft,columnCenter, columnRight);
+        this.gridPane.setVgap(10);
+
+        this.getChildren().add(this.gridPane);
+    }
+
+    private Label prettyLabel()
+    {
+        final Label subsystemLabel = new Label(this.subsystemName);
+
+        subsystemLabel.setFont(Font.font(Style.getFont(), FontWeight.BOLD, 16));
+        subsystemLabel.setTextFill(Style.darkAquaBlue.toColor());
+
+        return subsystemLabel;
+    }
+
+    /**
+     * @return the hasIssues
+     */
+    public boolean hasIssues()
+    {
+        return this.hasIssues;
+    }
+
+    /**
+     * Returns the verification issues for this subsystem
+     *
+     * @param geoPackage
+     *             GeoPackage to verify
+     * @return returns a {@link Collection} of {@link VerificationIssue}s
+     */
+    public Collection<VerificationIssue> getIssues(final GeoPackage geoPackage)
+    {
+        final Collection<VerificationIssue> issues = this.issuesFunction.apply(geoPackage);
+
+        this.hasIssues = !issues.isEmpty();
+
+        return issues;
+    }
+
+    /**
+     * Processes the verification issues, and updates the UI as necessary
+     *
+     * @param issues
+     *             Verification issues for the subsystem of a GeoPackage
+     */
+    public void update(final Collection<VerificationIssue> issues)
+    {
+        this.gridPane.getChildren().remove(this.progressIndicator);
+
+        if(issues != null && !issues.isEmpty())
+        {
+            final TextFlow textBox = new TextFlow();
+
+            textBox.setStyle(String.format("-fx-border-radius: 10 10 10 10; -fx-border-color: gray;-fx-background-radius: 10 10 10 10; -fx-background-color: %s;", Style.darkAquaBlue.getHex()));
+
+            for(final VerificationIssue issue : issues)
+            {
+                final Text severity    = getSeverityLabel   (issue.getRequirement().severity());
+                final Text requirement = getRequirementLabel(issue.getRequirement());
+                final Text reason      = getReasonLabel     (issue.getReason());
+
+                textBox.getChildren().addAll(severity, requirement, reason);
+            }
+
+
+            this.gridPane.add(getSeverityLevel(issues), 1, 0);
+            this.gridPane.add(this.createClipBoardButton(issues), 2, 0);
+            this.getChildren().add(textBox);
+        }
+        else
+        {
+            //add pass
+            this.gridPane.add(getPassText(), 1, 0);
+
+        }
+        this.snapshot(new SnapshotParameters(), new WritableImage(1,1));//added to refresh scroll pane
+
+    }
+
+    private Node createClipBoardButton(final Collection<VerificationIssue> issues)
+    {
+        final Image     clipBoard     = new Image(SubsystemVerificationPane.class.getResourceAsStream("Clipboard_Icon.png"));
+
+        final ImageView clipBoardView = new ImageView(clipBoard);
+        clipBoardView.setFitHeight(19);
+        clipBoardView.setFitWidth(19);
+
+        final Button    copyButton    = new Button();
+        copyButton.setMaxSize(24, 24);
+        copyButton.setMinSize(24, 24);
+        copyButton.setStyle(String.format("-fx-border-radius: 2 2 2 2;"
+                                        + "-fx-background-radius: 2 2 2 2; "));
+
+        final Tooltip copyMessage = new Tooltip("Copy Message");
+        Tooltip.install(copyButton, copyMessage);
+
+        copyButton.setGraphic(clipBoardView);
+        copyButton.setOnAction(e-> {
+                                        final Clipboard clipboard = Clipboard.getSystemClipboard();
+                                        final ClipboardContent content = new ClipboardContent();
+                                        content.putString(this.getVerificationIssues(issues));
+                                        clipboard.setContent(content);
+                                    });
+
+        return copyButton;
+    }
+
+    private String getVerificationIssues(final Collection<VerificationIssue> issues)
+    {
+          final String failedMessages = issues.stream()
+                                        .sorted((requirement1, requirement2) -> Integer.compare(requirement1.getRequirement().number(), requirement2.getRequirement().number()))
+                                        .map(issue -> {
+                                                        return String.format("(%s) Requirement %d: \"%s\"\n\n%s\n",
+                                                                             issue.getRequirement().severity(),
+                                                                             issue.getRequirement().number(),
+                                                                             issue.getRequirement().text(),
+                                                                             issue.getReason());
+                                                      })
+                                        .collect(Collectors.joining("\n"));
+
+         return  String.format("RGi GeoPackage Verfier Tool Version %.1f.\n\n\nGeoPackage failed to meet the following requirements for GeoPackage %s:\n\n%s",
+                                VerifierMainWindow.versionNumber,
+                                this.subsystemName,
+                                failedMessages);
+    }
+
+    private static Node getSeverityLevel(final Collection<VerificationIssue> issues)
+    {
+        final boolean hasError = issues.stream().anyMatch(issue -> issue.getRequirement().severity().equals(Severity.Error));
+
+        if(hasError)
+        {
+            return getSeverityLabel(Severity.Error);
+        }
+
+        return getSeverityLabel(Severity.Warning);
+    }
+
+    private static Text getPassText()
+    {
+        final Text passed = new Text("Passed");
+
+        passed.setFont(Font.font(Style.getFont(), FontWeight.BOLD, 16));
+        passed.setFill(Style.brightGreen.toColor());
+
+        return passed;
+    }
+
+    private static Text getSeverityLabel(final Severity severity)
+    {
+        final Text text = new Text(String.format("%s ",severity.name()));
+
+        text.setFill(getColor(severity));
+        text.setFont(Font.font(Style.getFont(), FontWeight.BOLD, 14));
+
+        return text;
+    }
+
+    private static Color getColor(final Severity severity)
+    {
+        switch(severity)
+        {
+            case Warning: return Style.brightOrange.toColor();
+            case Error:   return Style.brightRed.toColor();
+
+            default: return Color.BLACK;
+        }
+    }
+
+    private static Text getRequirementLabel(final Requirement requirement)
+    {
+        final Text text = new Text();
+
+        text.setText(String.format(" Requirement %d: \"%s\"\n",
+                                    requirement.number(),
+                                    requirement.text()));
+
+        text.setFont(Font.font(Style.getFont(), FontWeight.EXTRA_BOLD, 12));
+        text.setFill(Style.white.toColor());
+
+        return text;
+    }
+
+    private static Text getReasonLabel(final String reason)
+    {
+        final Text text = new Text(String.format("%s \n", reason));
+        text.setFill(Style.greyBlue.toColor());
+        text.setFont(Font.font(Style.getFont(), FontWeight.BOLD, 12));
+
+        return text;
+    }
+}
