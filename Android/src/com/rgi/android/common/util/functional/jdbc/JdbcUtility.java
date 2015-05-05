54b512d68b627a9baaa46cc80863d49a04f6d169
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-05-05 08:58:24 -0400

    adding javadoc to jdbcUtility

--- Android/src/com/rgi/android/common/util/functional/jdbc/JdbcUtility.java
+++ Android/src/com/rgi/android/common/util/functional/jdbc/JdbcUtility.java
@@ -12,14 +12,14 @@ public class JdbcUtility
 {
     /**
      * Returns {@Link ArrayList} of the type of the input consisting of the results of applying the
-     *  operations in {@Link ResultSetMapper} on the given {@Link ResultSet}
+     *  operations in {@Link ResultSetPredicate} on the given {@Link ResultSet}
      *
      * @param resultSet
-     *      The result set to perform the operations on
-     * @param resultSetMapper
-     *      the definition of the operations performed on a {@Link ResultSet}
+     *      The result set containing the elements
+     * @param resultSetPredicate
+     *      Evaluates each element in the given {@Link ResultSet} to see if it satisfies the predicate
      * @return
-     *      an ArrayList of the type of input to the operation
+     *      an ArrayList of the type of input consisting of the elements that satisfy the resultSetPredicate
      * @throws SQLException
      *      throws if an SQLException occurs
      */
@@ -47,10 +47,17 @@ public class JdbcUtility
     }
 
     /**
+     *  Returns {@Link ArrayList} of the type of the input consisting of the results of applying the
+     *  operations in {@Link ResultSetMapper} on the given {@Link ResultSet}
+     *
      * @param resultSet
+     *      The result set consisting of the elements
      * @param resultSetMapper
+     *      Maps the given {@Link ResultSet} elements and outputs to the same type of input based on the apply function
      * @return
+     *      An {@Link ArrayList} of the type of the input that are the results of the mapping the elements in the given {@Link ResultSet}
      * @throws SQLException
+     *      throws if an SQLException occurs
      */
     public static <T> ArrayList<T> map(final ResultSet resultSet, final ResultSetMapper<T> resultSetMapper) throws SQLException
     {
