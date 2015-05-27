637911f6a594d7f2cc2bfb519c4780a5b4a9f895
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-05-27 14:45:53 -0400

    adding else clause to initialize an empty Set TilesVerifier

--- GeoPackage/src/com/rgi/geopackage/tiles/TilesVerifier.java
+++ GeoPackage/src/com/rgi/geopackage/tiles/TilesVerifier.java
@@ -33,6 +33,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -161,6 +162,10 @@ public class TilesVerifier extends Verifier
                                                                  .filter(Objects::nonNull)
                                                                  .collect(Collectors.toSet());
             }
+        }
+        else
+        {
+            this.pyramidTablesInTileMatrix = Collections.emptySet();
         }
 
     }
