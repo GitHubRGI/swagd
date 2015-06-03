a8c4ae474120c2dbb7e959b639aca5612ba6de0b
Mary Carome <maryc13@vt.edu>
2015-06-03 13:20:17 -0400

    Added function to set PRAGMA synchronous to OFF

--- Android/src/com/rgi/android/geopackage/GeoPackage.java
+++ Android/src/com/rgi/android/geopackage/GeoPackage.java
@@ -202,6 +202,7 @@ public class GeoPackage implements Closeable
 
             DatabaseUtility.setPragmaForeignKeys(this.databaseConnection, true);
             DatabaseUtility.setPragmaJournalModeMemory(this.databaseConnection);
+            DatabaseUtility.setPragmaSynchronousOff(this.databaseConnection);
 
             this.core       = new GeoPackageCore      (this.databaseConnection, isNewFile);
             this.features   = new GeoPackageFeatures  (this.databaseConnection, this.core);
