aebdc85762d317553283b17b28c2d983e204c0f6
Mary Carome <maryc13@vt.edu>
2015-06-05 15:19:09 -0400

    Added unit tests/deleted Main.java

--- NetworkExtension/test/com/rgi/geopackage/extensions/network/GeoPackageNetworkExtensionTest.java
+++ NetworkExtension/test/com/rgi/geopackage/extensions/network/GeoPackageNetworkExtensionTest.java
@@ -20,8 +20,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package com.rgi.geopackage.extensions.network;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,22 +33,128 @@ import org.junit.Test;
 
 import utility.TestUtility;
 
+import com.rgi.common.BoundingBox;
 import com.rgi.geopackage.GeoPackage;
+import com.rgi.geopackage.extensions.Scope;
+import com.rgi.geopackage.extensions.implementation.BadImplementationException;
 import com.rgi.geopackage.verification.ConformanceException;
 
+/**
+ * Unit tests for GeoPackageNetworkExtensions
+ *
+ * @author Mary Carome
+ *
+ */
 @SuppressWarnings({"static-method"})
 public class GeoPackageNetworkExtensionTest
 {
+	/**
+	 * Tests getExtensionName
+	 */
 	@Test
-     public void testGetExtensionName()
-     {
-         final File testFile = TestUtility.getRandomFile(10);
-         try(GeoPackage gpkg = new GeoPackage(testFile))
-         {
-        	 //TODO
-         } catch (ClassNotFoundException | SQLException | ConformanceException | IOException e)
+    public void testGetExtensionName()
+    {
+        final File testFile = TestUtility.getRandomFile(10);
+        try(GeoPackage gpkg = new GeoPackage(testFile))
+        {
+        	final String networkName = "rgi_network";
+       	    final GeoPackageNetworkExtension networkExtension = gpkg.extensions().getExtensionImplementation(GeoPackageNetworkExtension.class);
+       	    assertTrue(String.format("GeopackageNetworkExtensionName method getExtensionName returns %s instead of ", networkExtension.getExtensionName(), networkName),
+       	    		   networkExtension.getExtensionName().equals(networkName));
+        }
+        catch (ClassNotFoundException | SQLException | ConformanceException | IOException | BadImplementationException e)
 		{
 			e.printStackTrace();
 		}
-     }
+    }
+
+	/**
+	 * Tests getDefinition
+	 */
+	@Test
+	public void testGetDefinition()
+	{
+        final File testFile = TestUtility.getRandomFile(5);
+        try(GeoPackage gpkg = new GeoPackage(testFile))
+        {
+        	final String definition = "definition";
+       	    final GeoPackageNetworkExtension networkExtension = gpkg.extensions().getExtensionImplementation(GeoPackageNetworkExtension.class);
+       	    assertTrue(String.format("GeopackageNetworkExtensionName method getDefintion returns %s instead of %s", networkExtension.getDefinition(), definition),
+       	    		   networkExtension.getDefinition().equals(definition));
+        }
+        catch (ClassNotFoundException | SQLException | ConformanceException | IOException | BadImplementationException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Tests getDefinition
+	 */
+	@Test
+	public void testGetScope()
+	{
+        final File testFile = TestUtility.getRandomFile(7);
+        try(GeoPackage gpkg = new GeoPackage(testFile))
+        {
+       	    final GeoPackageNetworkExtension networkExtension = gpkg.extensions().getExtensionImplementation(GeoPackageNetworkExtension.class);
+       	    assertTrue(String.format("GeopackageNetworkExtensionName method getScope returns %s instead of %s", networkExtension.getScope(), Scope.ReadWrite),
+       	    		   networkExtension.getScope().equals(Scope.ReadWrite));
+        }
+        catch (ClassNotFoundException | SQLException | ConformanceException | IOException | BadImplementationException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Tests that getNodeAttributesTableName name throws an IllegalArgumentException
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void illegalArgumentGetNodeAttributesTableName1()
+	{
+		final Network network = null;
+        GeoPackageNetworkExtension.getNodeAttributesTableName(network);
+        fail("Expected GeoPackageNetworkExtension method getNodeAttributesTableName(Network) to throw an exception when passed a null Network");
+	}
+
+	/**
+	 * Tests that getNodeAttributesTableName name throws an IllegalArgumentException
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void illegalArgumentGetNodeAttributesTableName2()
+	{
+		final String network = null;
+        GeoPackageNetworkExtension.getNodeAttributesTableName(network);
+        fail("Expected GeoPackageNetworkExtension method getNodeAttributesTableName(String) to throw an exception when passed a null String");
+	}
+
+	/**
+	 * Test that getNodeAttributesTableName correctly return the table name
+	 * when passed a network
+	 */
+	public void getNodeAttributesTableName1()
+	{
+ 	    final String tableName = "my_table_node_attributes";
+        final Network network = new Network("my_table", "test", "test", "test", new BoundingBox(0,0,0,0), 0);
+
+   	    assertTrue(String.format("GeoPackageNetworkExtension method getNodeAttributesTableName returned %s, but %s was expected",
+   	    		                  GeoPackageNetworkExtension.getNodeAttributesTableName(network),
+   	    		                  tableName),
+   	    		   GeoPackageNetworkExtension.getNodeAttributesTableName(network).equals(tableName));
+	}
+
+	/**
+	 * Test that getNodeAttributesTableName correctly return the table name
+	 * when passed a non-null String
+	 */
+	public void getNodeAttributesTableName2()
+	{
+ 	    final String tableName = "my_table_node_attributes";
+
+   	    assertTrue(String.format("GeoPackageNetworkExtension method getNodeAttributesTableName returned %s, but %s was expected",
+   	    		                  GeoPackageNetworkExtension.getNodeAttributesTableName("my_table"),
+   	    		                  tableName),
+   	    		   GeoPackageNetworkExtension.getNodeAttributesTableName("my_table").equals(tableName));
+	}
 }
