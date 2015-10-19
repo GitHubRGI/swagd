e59807f9eafaf1841cea7ad1ae9553df4635483c
Steven D. Lander <steven.lander@rgi-corp.com>
2015-10-19 15:51:52 -0400

    change configure to script, not built-in

--- gdal_1.11.1_jni.sh
+++ gdal_1.11.1_jni.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -d "$HOME/gdal/swig" ]; then
     wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz;
     tar xzvf gdal-1.11.1.tar.gz;
-    cd gdal-1.11.1 && configure --prefix=$HOME/gdal && make && make install;
+    cd gdal-1.11.1 && ./configure --prefix=$HOME/gdal && make && make install;
     cd $HOME/gdal/swig/java && make;
 else
     echo 'Using cached directory.';
