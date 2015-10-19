85746ceb3a89da6b711bf3047ccf0d24c5a6126c
Steven D. Lander <steven.lander@rgi-corp.com>
2015-10-19 15:41:29 -0400

    switch gdal url to http (not https)

--- gdal_1.11.1_jni.sh
+++ gdal_1.11.1_jni.sh
@@ -2,7 +2,7 @@
 set -e
 # check to see if the gdal_1.11.1_jni folder is empty
 if [ ! -d "$HOME/gdal/swig" ]; then
-    wget https://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz;
+    wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz;
     tar xzvf gdal-1.11.1.tar.gz;
     cd gdal-1.11.1 && configure --prefix=$HOME/gdal && make && make install;
     cd $HOME/gdal/swig/java && make;
