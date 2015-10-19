2c8f435e76793a2b3c4cfb41c492676e0569596f
Steven D. Lander <steven.lander@rgi-corp.com>
2015-10-19 16:12:35 -0400

    gdal build tweaks

--- gdal_1.11.1_jni.sh
+++ gdal_1.11.1_jni.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 # check to see if the gdal_1.11.1_jni folder is empty
-if [ ! -d "$HOME/gdal/swig" ]; then
+if [ -d "$HOME/gdal/lib/swig/java" ]; then
+    echo "swig dir found"
+fi
+
+if [ ! -d "$HOME/gdal/lib" ]; then
     wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz;
     tar xzvf gdal-1.11.1.tar.gz;
     cd gdal-1.11.1 && ./configure --prefix=$HOME/gdal && make && make install;
-    cd $HOME/gdal/swig/java && make;
+    cd $HOME/gdal/lib/swig/java && make;
 else
     echo 'Using cached directory.';
 fi
