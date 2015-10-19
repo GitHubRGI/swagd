33b41b69c242e70a671e74916a3aa53de5affdfa
Steven D. Lander <steven.lander@rgi-corp.com>
2015-10-19 15:25:43 -0400

    add gdal jni make process

---
+++ gdal_1.11.1_jni.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+# check to see if the gdal_1.11.1_jni folder is empty
+if [ ! -d "$HOME/gdal" ]; then
+    wget https://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz;
+    tar xzvf gdal-1.11.1.tar.gz;
+    cd gdal-1.11.1 && configure --prefix=$HOME/gdal && make && make install;
+    cd $HOME/gdal/swig/java && make;
+else
+    echo 'Using cached directory.';
+fi
