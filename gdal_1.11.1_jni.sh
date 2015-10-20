127f43b94363b1416494b5b29bc90e1994aefeff
Steven D. Lander <steven.lander@rgi-corp.com>
2015-10-20 14:10:50 -0400

    remove jdk_switcher command from script

--- gdal_1.11.1_jni.sh
+++ gdal_1.11.1_jni.sh
@@ -12,7 +12,6 @@ if [ ! -d "$HOME/gdal/lib" ]; then
     # make the java bindings
     echo 'Making SWIG bindings for Java...';
     # Set java env vars
-    jdk_switcher use oraclejdk8
     export JAVA_HOME=/usr/lib/jvm/java-8-oracle
     export JAVA_INCLUDE=-I$(JAVA_HOME)/include -I$(JAVA_HOME)/include/linux;
     cd swig/java && make;
