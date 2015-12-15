3dd9b2937a02654424eb9861dcfd8f6aa7780e1d
Steven D. Lander <steven.lander@rgi-corp.com>
2015-12-15 09:29:59 -0500

    add Wix installer script

--- install-deps-windows.bat
+++ install-deps-windows.bat
@@ -3,11 +3,13 @@
 call mvn install:install-file -Dfile=RGISuite/lib/gdal.jar ^
     -DgroupId=org.osgeo -DartifactId=gdal -Dversion=1.11.1 ^
     -Dpackaging=jar
-echo Exit Code = %ERRORLEVEL%
-if not "%ERRORLEVEL%" == "0" exit /b
+SET result=%ERRORLEVEL%
+echo Exit Code = %result%
+if not "%result%" == "0" exit /b
 
 call mvn install:install-file -Dfile=GeoViewer/lib/JMapViewer-1.05.jar ^
     -DgroupId=org.openstreetmap -DartifactId=JMapViewer ^
     -Dversion=1.05 -Dpackaging=jar
-echo Exit Code = %ERRORLEVEL%
-if not "%ERRORLEVEL%" == "0" exit /b
+SET result=%ERRORLEVEL%
+echo Exit Code = %result%
+if not "%result%" == "0" exit /b
